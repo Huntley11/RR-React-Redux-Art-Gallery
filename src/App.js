136f5005399333ab66a00a2eab966cdb6786ee95
Huntley <huntleycastner@gmail.com>
2023-04-27 21:18:36 -0700

    finished day 10 activity

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import './App.css';
 import { useSelector, useDispatch, connect } from 'react-redux'
-import { clearData, fetchData, incrementId, decrementId, inputId } from './features/dataSlice'
+import { reset, fetchData, nextImage, prevImage, setArtId } from './features/dataSlice'
 import { useEffect } from 'react';
 
-function App(props) {
-  const dispatch = useDispatch()
-  const data = useSelector((state) => state.data)
+const mapStateToProps = state => {
+  return state.data
+};
 
-  const renderImg = () => {
-    if(data.apiData) {
-      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
-    } else {
-      return <p>image here</p>
-    }
-  }
+function App({ artId, isLoggedIn }) {
+  const dispatch = useDispatch()
+  const currentState = useSelector(state => state.data)
+
 
   useEffect(() => {
     dispatch(fetchData())
-  }, [props.objectId, dispatch])
+  }, [artId, dispatch])
 
+  const renderImg = () => {
+    return currentState.apiData ?
+      <img src={currentState.apiData.primaryImage} />
+      :
+      <h3>No image available</h3>
+  }
 
   return (
-    <div className="App">
-      <div>
-        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
-        <button onClick={() => dispatch(clearData())}>Clear</button>
-        <button onClick={() => dispatch(incrementId())}>Next</button>
-        <button onClick={() => dispatch(decrementId())}>Back</button>
+    <div className="App" style={{ 'backgroundColor': isLoggedIn ? 'red' : 'yellow'}}>
+      <div className='button-container'>
+        <button onClick={() => dispatch(fetchData())}>Trigger Thunk!</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => dispatch(nextImage())}>Next</button>
+        <button onClick={() => dispatch(prevImage())}>Back</button>
       </div>
-      <input value={ data.objectId } onChange={(e) => {
-        dispatch(inputId(Number(e.target.value)))
-      }} />
-      <div>
-        {data.objectId}
+      <div className='input-controls'>
+        <h1>{ currentState.artId }</h1>
+        <input value={currentState.artId} onChange={(e) => {
+          dispatch(setArtId(Number(e.target.value)))
+        }} />
+      </div>
+      <div className='image-container'>
+        {currentState.artId}
         {renderImg()}
       </div>
     </div>
   );
 }
-
-
-const mapStateToProps = (state, ownProps) => ({ objectId: state.data.objectId })
 
 export default connect(mapStateToProps)(App);
